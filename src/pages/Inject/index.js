11f1e8aaffb0e95ef65e1468b9ee3f708c75a9cf
Liam Merlyn <l.merlyn@hotmail.co.uk>
2024-03-31 14:13:38 +0100

    Minor code updates

--- src/pages/Inject/index.js
+++ src/pages/Inject/index.js
@@ -1,28 +1,27 @@
 function getThemeDataAndSendItToExt() {
   console.log('thing');
-  if (window.location.href.indexOf("admin.shopify.com/store") > -1) return;
+  if (window.location.href.includes('admin.shopify.com/store')) return;
 
   console.log('Shopify', window.Shopify);
-  if ((typeof Shopify !== 'undefined' && typeof Shopify.theme !== 'undefined') || (typeof Shopify !== 'undefined' && typeof Shopify.shop !== 'undefined')) {
+  if (window.Shopify && (Shopify.theme || Shopify.shop)) {
     console.log('Shopify', Shopify);
-    window.postMessage(
-      {
-        type: 'theme',
-        data: {
-          shopify: 'Shopify',
-          theme: {
-            handle: Shopify.theme.handle,
-            id: Shopify.theme.id,
-            name: Shopify.theme.name,
-            role: Shopify.theme.role,
-          },
-          shop: Shopify.shop,
-          shopHandle: Shopify.shop,
-          location: window.location.href
-        },
+    const themeData = {
+      type: 'theme',
+      data: {
+        theme: Shopify.theme
+          ? {
+              handle: Shopify.theme.handle,
+              id: Shopify.theme.id,
+              name: Shopify.theme.name,
+              role: Shopify.theme.role,
+            }
+          : undefined,
+        shop: Shopify.shop,
+        location: window.location.href,
       },
-      '*'
-    );
+    };
+
+    window.postMessage(themeData, window.location.origin);
   }
 }
 
