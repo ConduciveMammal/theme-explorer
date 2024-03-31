e2d931163b01084b87ff4d29363c2f072855393e
Liam Merlyn <l.merlyn@hotmail.co.uk>
2024-03-31 14:40:54 +0100

    Updated UI when themes are processing

--- src/containers/ThemeAccordion/ThemeAccordion.jsx
+++ src/containers/ThemeAccordion/ThemeAccordion.jsx
@@ -14,91 +14,101 @@ const ThemeAccordion = ({ theme, shop, index, storeUrl, ...additionalAttrs }) =>
   const themeCodeUrl = `${storeUrl}/themes/${theme.id}`;
   const themeCustomiseUrl = `${storeUrl}/themes/${theme.id}/editor`;
   const themeLanguageEditorUrl = `${storeUrl}/themes/${theme.id}/language`;
+  const processing = theme.processing
 
   return (
-    <div className="Accordion__Container">
-      <header
-        className={`Accordion__Header${
-          theme.role === 'main' ? ' Accordion__Header--main' : ''
-        }${isOpen ? ' Accordion__Header--open' : ''}`}
-        onClick={onToggle}
-        aria-controls={`Accordion-${index}`}
-        {...additionalAttrs}
-      >
-        <div className="Accordion__Icon-container">
-          <Icon
-            name="theme"
-            color="#FFFFFF"
-            size={35}
-            classes={'Accordion__Icon'}
+      <div className={`Accordion__Container${processing ? ' Accordion__Container--processing': ''}`}>
+        <header
+          className={`Accordion__Header${
+            theme.role === 'main' ? ' Accordion__Header--main' : ''
+          }${isOpen ? ' Accordion__Header--open' : ''}`}
+          onClick={processing ? null : onToggle}
+          aria-controls={`Accordion-${index}`}
+          {...additionalAttrs}
+        >
+          <div className="Accordion__Icon-container">
+            <Icon
+              name="theme"
+              color="#FFFFFF"
+              size={35}
+              classes={'Accordion__Icon'}
+            />
+          </div>
+          <p className="Accordion__Title">{theme.name}</p>
+          <p className="Accordion__Link">
+            {processing ?
+            <Icon
+            name="spinner"
+            color="#b3b8ff"
+            size={24}
+            classes={'Accordion__Spinner'}
           />
-        </div>
-        <p className="Accordion__Title">{theme.name}</p>
-        <p className="Accordion__Link">
-          <a
-            href={
-              theme.role === 'main' ? `https://${shop.domain}` : themePreviewUrl
+            :
+            <a
+              href={
+                theme.role === 'main' ? `https://${shop.domain}` : themePreviewUrl
+              }
+              target="_blank"
+              rel="noreferrer"
+            >
+              {theme.role === 'main' ? 'View' : 'Preview'}
+            </a>
             }
-            target="_blank"
-            rel="noreferrer"
-          >
-            {theme.role === 'main' ? 'View' : 'Preview'}
-          </a>
-        </p>
-      </header>
-      <Collapse
-        id={`Accordion-${index}`}
-        className="Accordion__Body"
-        aria-hidden={isOpen ? 'false' : 'true'}
-        isOpen={isOpen}
-        transition={`height .2s cubic-bezier(.4, 0, .2, 1)`}
-      >
-        <div className="Accordion__Body__Content">
-          <p className="Accordion__Detail">
-            <span className="Accordion__Label">Theme ID: </span>
-            <span className="Accordion__Value">{theme.id}</span>
           </p>
-          <p className="Accordion__Detail">
-            <span className="Accordion__Label">Role: </span>
-            <span className="Accordion__Value">{theme.role}</span>
-          </p>
-          <p className="Accordion__Detail">
-            <span className="Accordion__Label">Updated at: </span>
-            <span className="Accordion__Value">
-              <DisplayDate date={theme.created_at} />
-            </span>
-          </p>
-          <p className="Accordion__Detail">
-            <span className="Accordion__Label">Created at: </span>
-            <span className="Accordion__Value">
-              <DisplayDate date={theme.created_at} />
-            </span>
-          </p>
-          <footer className="Accordion__Footer">
-            <p className="Footer__Link">
-              <a href={themeJsonUrl} target="_blank" rel="noreferrer">
-                View JSON
-              </a>
+        </header>
+        <Collapse
+          id={`Accordion-${index}`}
+          className="Accordion__Body"
+          aria-hidden={isOpen ? 'false' : 'true'}
+          isOpen={isOpen}
+          transition={`height .2s cubic-bezier(.4, 0, .2, 1)`}
+        >
+          <div className="Accordion__Body__Content">
+            <p className="Accordion__Detail">
+              <span className="Accordion__Label">Theme ID: </span>
+              <span className="Accordion__Value">{theme.id}</span>
             </p>
-            <p className="Footer__Link">
-              <a href={themeCustomiseUrl} target="_blank" rel="noreferrer">
-                Customise
-              </a>
+            <p className="Accordion__Detail">
+              <span className="Accordion__Label">Role: </span>
+              <span className="Accordion__Value">{theme.role}</span>
             </p>
-            <p className="Footer__Link">
-              <a href={themeCodeUrl} target="_blank" rel="noreferrer">
-                Edit code
-              </a>
+            <p className="Accordion__Detail">
+              <span className="Accordion__Label">Updated at: </span>
+              <span className="Accordion__Value">
+                <DisplayDate date={theme.created_at} />
+              </span>
             </p>
-            <p className="Footer__Link">
-              <a href={themeLanguageEditorUrl} target="_blank" rel="noreferrer">
-                Edit languages
-              </a>
+            <p className="Accordion__Detail">
+              <span className="Accordion__Label">Created at: </span>
+              <span className="Accordion__Value">
+                <DisplayDate date={theme.created_at} />
+              </span>
             </p>
-          </footer>
-        </div>
-      </Collapse>
-    </div>
+            <footer className="Accordion__Footer">
+              <p className="Footer__Link">
+                <a href={themeJsonUrl} target="_blank" rel="noreferrer">
+                  View JSON
+                </a>
+              </p>
+              <p className="Footer__Link">
+                <a href={themeCustomiseUrl} target="_blank" rel="noreferrer">
+                  Customise
+                </a>
+              </p>
+              <p className="Footer__Link">
+                <a href={themeCodeUrl} target="_blank" rel="noreferrer">
+                  Edit code
+                </a>
+              </p>
+              <p className="Footer__Link">
+                <a href={themeLanguageEditorUrl} target="_blank" rel="noreferrer">
+                  Edit languages
+                </a>
+              </p>
+            </footer>
+          </div>
+        </Collapse>
+      </div>
   );
 };
 
